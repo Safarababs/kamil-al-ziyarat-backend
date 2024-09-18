1a3506a26710cca9b6b93ceeacc81c4e147b857e
SAFAR ABBAS <safarabbas73.sa@gmail.com>
2024-09-18 21:50:14 +0300

    safar

--- routes/hadithRoutes.js
+++ routes/hadithRoutes.js
@@ -21,6 +21,7 @@ router.get("/get-hadiths", async (req, res) => {
   }
 });
 
+// Add a new hadith
 router.post("/add-hadith", async (req, res) => {
   try {
     const hadith = new Hadith(req.body);
@@ -29,6 +30,57 @@ router.post("/add-hadith", async (req, res) => {
   } catch (error) {
     console.error("Error adding Hadith:", error);
     res.status(500).json({ message: "Failed to add Hadith" });
+  }
+});
+
+// Update a hadith by ID
+router.put("/update-hadith/:id", async (req, res) => {
+  const { id } = req.params;
+  const updateData = req.body;
+
+  console.log("Received ID:", id);
+  console.log("Received update data:", updateData); // Log the incoming data
+
+  if (!updateData || Object.keys(updateData).length === 0) {
+    return res.status(400).json({ message: "No update data provided" });
+  }
+
+  try {
+    const updatedHadith = await Hadith.findByIdAndUpdate(id, updateData, {
+      new: true,
+      runValidators: true, // Ensures that the update adheres to schema validation
+    });
+
+    if (!updatedHadith) {
+      return res.status(404).json({ message: "Hadith not found" });
+    }
+    res.json(updatedHadith);
+  } catch (error) {
+    console.error("Error updating Hadith:", error); // Log the actual error
+    res
+      .status(500)
+      .json({ message: "Error updating Hadith", error: error.message });
+  }
+});
+
+// DELETE a hadith by ID
+// DELETE a hadith by ID
+router.delete("/delete-hadith/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedHadith = await Hadith.findByIdAndDelete(id);
+
+    if (!deletedHadith) {
+      console.log(`Hadith with ID ${id} not found.`);
+      return res.status(404).json({ message: "Hadith not found" });
+    }
+
+    console.log(`Hadith with ID ${id} deleted successfully.`);
+    res.status(200).json({ message: "Hadith deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting Hadith:", error);
+    res.status(500).json({ message: "Server error" });
   }
 });
 
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const hadithRoutes = require("./routes/hadithRoutes");
 const chapterRoutes = require("./routes/chapterRoutes");
 
@@ -12,11 +11,11 @@ const PORT = 5000;
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://kamil-al-ziyarat.netlify.app"],
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
 );
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Routes
 app.use("/api", hadithRoutes); // Ensure '/api' matches the route in frontend fetch
